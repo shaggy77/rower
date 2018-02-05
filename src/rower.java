16d97f6653034ada8f3136fe35aeed89d61f45db
Wojciech <wojciechmikolajzajac@gmail.com>
2018-02-05 20:46:28 +0100

    Initial commit

---
+++ src/rower.java
@@ -0,0 +1,9 @@
+/* Zaprojektuj rower. Niech ma obiekt kierownica, pedały, łańcuch i koła. Obiekt koło niech dziedziczy po klasie ogólnej,
+a w swoich szczególnych interpretacjach niech różni się tym, czym w rzeczywistości różni się koło przednie od tylnego.
+Zakładamy, że pedały są takie same.
+*/
+public class rower {
+    public static void main(String[] args) {
+         
+    }
+}
